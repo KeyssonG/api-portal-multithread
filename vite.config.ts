1b38c6c1c0fe0558397b6e4478497100362157d4
keysson Gomes <104814645+KeyssonG@users.noreply.github.com>
2025-06-22 14:11:25 -0300

    Instalação de certificação

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
-// https://vite.dev/config/
+const certPath = path.resolve(__dirname, './ssl/cert.pem')
+const keyPath = path.resolve(__dirname, './ssl/key.pem')
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      cert: fs.readFileSync(certPath),
+      key: fs.readFileSync(keyPath)
+    },
+    port: 5174,
+    host: '0.0.0.0'
+  }
 })
