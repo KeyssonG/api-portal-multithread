87aa10fd18e704fa61eb5a4866a53e685b6ae125
keysson Gomes <104814645+KeyssonG@users.noreply.github.com>
2025-07-09 19:12:41 -0300

    Remoção de lógica de https, mantendo apenas http

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
-
-const certPath = path.resolve(__dirname, './ssl/cert.pem')
-const keyPath = path.resolve(__dirname, './ssl/key.pem')
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      cert: fs.readFileSync(certPath),
-      key: fs.readFileSync(keyPath)
-    },
     port: 5174,
     host: '0.0.0.0'
   }
